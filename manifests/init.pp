d87bebc369be99dfbd23813625b565fe901001dc
Elle K <elle@linuxacademy.com>
2019-05-29 11:13:20 -0400

    Spacing issues

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,12 +3,12 @@
 class mysql (
   $install_name   = $mysql::params::install_name,
   $install_ensure = $mysql::params::install_ensure,
-   $config_path   = $mysql::params::config_path,
-   $config_ensure = $mysql::params::config_ensure,
-   $service_name       = $mysql::params::service_name,
-   $service_ensure     = $mysql::params::service_ensure,
-   $service_enable     = $mysql::params::service_enable,
-   $service_hasrestart = $mysql::params::service_hasrestart,
+  $config_path   = $mysql::params::config_path,
+  $config_ensure = $mysql::params::config_ensure,
+  $service_name       = $mysql::params::service_name,
+  $service_ensure     = $mysql::params::service_ensure,
+  $service_enable     = $mysql::params::service_enable,
+  $service_hasrestart = $mysql::params::service_hasrestart,
 ) inherits mysql::params {
   contain mysql::install
   contain mysql::config
